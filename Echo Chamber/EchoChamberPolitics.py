9c096a397046c230ff7e16b3aa4558cfbd53f3ff
leakyhose <72418700+leakyhose@users.noreply.github.com>
2023-01-12 17:17:57 -0800

    Update EchoChamberPolitics.py

--- Echo Chamber/EchoChamberPolitics.py
+++ Echo Chamber/EchoChamberPolitics.py
@@ -1,28 +1,59 @@
-import snscrape.modules.twitter as sntwitter
-import json
-import csv
-import math
-from urllib.parse import urlparse
 import networkx as nx
 import matplotlib.pyplot as plt
-ego = 'AllenLallen81'
+import tweepy
+import json
+from accuracyTest import get_political_score
+#Rory__gilmartin
+ego = "AllenLallen81"
+graph = {}
+graph[ego] = []
+client = tweepy.Client(bearer_token='AAAAAAAAAAAAAAAAAAAAAFj0hwEAAAAACZOCgk6vyIaWdJvXMzG%2BNjYK75o%3D3GKg59g2YXoFcbKD5oyL5oP8rwfTw0kGGbtRd9MpLForplCj8Q',wait_on_rate_limit=True)
 
-color_map = []
-biased_news_scores = {}
-biased_hashtags_scores = {}
+consumer_key = "cBFx745dKpTo1emtbhgJr1mAZ" 
+consumer_secret = "WFFElpiFHCs9CB0JXnVQTDcNpsqW6VsJ7OV97ugD0vI5qCuoSM" 
+access_token = "1299158638217453569-9DTfODUeB5y97e2YMoDrrsxjUClmVC" 
+access_secret = "3ZqJgunwK6ML5GIL9j3xcnd974zw89ZctzDYJWB1lV5GJ"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+ 
+auth.set_access_token(access_token, access_secret)
+ 
+api = tweepy.API(auth,wait_on_rate_limit=True)
+
+G = nx.Graph()
+
+def get_followers(name, n):
+    info = client.get_user(username=name)
+    user_id = info.data.id
+    followers = client.get_users_followers(id=user_id, max_results=n,user_fields=["username"])
+    return followers
+
+def get_following(name, n):
+    info = client.get_user(username=name)
+    user_id = info.data.id
+    following = client.get_users_following(id=user_id, max_results=n,user_fields=["username"])
+    return following
+
+def remove_single_element_lists(dictionary):
 
 
-with open('graph.json', 'r', encoding='utf-8') as outfile:
-    graph = json.load(outfile)
-with open('hashtags.csv', 'r', encoding='utf-8') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        biased_hashtags_scores[row[0]] = row[1]
-with open('bias_scores.csv', 'r') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        biased_news_scores[row[0]] = row[1]
+    keys_to_remove = []
+    for key, value in dictionary.items():
+        if isinstance(value, list) and len(value) == 1:
+            keys_to_remove.append(key)
+    for key in keys_to_remove:
+        del dictionary[key]
+    return dictionary
 
+def findFollowers(ego):
+    followerData = get_followers(ego, 100).data
+    followingData = get_following(ego, 100).data
+
+
+    for follower in followerData:
+        graph[ego].append(follower.username)
+    for following in followingData:
+        graph[ego].append(following.username)
 
 def colorConvert(score):
     if score > 0:
@@ -32,33 +63,26 @@ def colorConvert(score):
     else:
         return [0,0,0,0]
 
+findFollowers(ego)
+checked = []
 
-def get_political_score(handle, n):
-    score = 0.0
-    count = 0
-    for i, tweet in enumerate(sntwitter.TwitterSearchScraper('(from:'+ handle + ') AND (filter:links OR filter:hashtags) AND include:nativeretweets').get_items()):
-        count += 1
-        if i > n:
-            break
 
-        if tweet.links:
-            for link in tweet.links:
-                web_domain = urlparse(link.url).netloc.replace("www.", "").lower()
+count = 1
+for name in graph[ego]:
+    graph[name] = [ego]
+for source in graph.keys():
+    for target in graph.keys():
 
-                if web_domain in biased_news_scores:
-                    score += float(biased_news_scores[web_domain])
+        if target not in checked and target != source:
+            friendship = api.get_friendship(source_screen_name = source, target_screen_name = target)
+            print(count)
+            count += 1
+            if friendship[0].followed_by == True or friendship[0].following == True:
+                    graph[source].append(target)
+        checked.append(source)
 
-        if tweet.hashtags:
-            for hashtag in tweet.hashtags:
-                if hashtag.lower() in biased_hashtags_scores:
-                    score += float(float(biased_hashtags_scores[hashtag.lower()]) * 1.5)
-    if count < 5:
-        return 0
-    if score >= 0:
-        return 1 - math.exp(-0.05 * score)
-    else:
-        return -(1 - math.exp(0.05 * score))
-
+  
+color_map = []
 for key in graph.keys():
     color_map.append(colorConvert(get_political_score(key,1500)))
     print(key)
@@ -89,4 +113,3 @@ nx.draw_networkx(hub_ego, pos,node_size=[v * bing for v in d.values()],with_labe
 nx.draw_networkx_nodes(hub_ego,pos,nodelist=[ego],node_size=[v * bing for v in d.values()][0],node_color=(0,1,0),edgecolors="black",linewidths=1)
 plt.axis("off")
 plt.show()
-
